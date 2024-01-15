5f0606c320de93548b2bc1c8fdd2bb05ee3c6849
Ross Gray <rossgray@TechEd-0008.techeducators.co.uk>
2024-01-15 13:32:32 +0000

    first

---
+++ app.js
@@ -0,0 +1,11 @@
+const favouriteFoods = ["Hamburgers", "Fries", "Sushi"];
+
+console.log(favouriteFoods);
+
+const favouriteColours = ["Green", "Teal", "Mauve"];
+
+console.log(favouriteColours);
+
+const favouriteNumbers = [42, 45, 48];
+
+console.log(favouriteNumbers);
